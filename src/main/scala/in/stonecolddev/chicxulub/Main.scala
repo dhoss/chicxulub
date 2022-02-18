a967974c63c641c10802456639112f7e1e7f8ed7
Devin Austin <devin.austin@gmail.com>
2022-02-18 16:40:32 -0700

    added scalatags, scalatra dependencies
    swapped out scalatra for cask, added scalatags, implemented initial http server with root endpoint

--- src/main/scala/in/stonecolddev/chicxulub/Main.scala
+++ src/main/scala/in/stonecolddev/chicxulub/Main.scala
@@ -1,7 +1,33 @@
 package in.stonecolddev.chicxulub
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("hello there")
+import scalatags.Text.all._
+
+object Main extends cask.MainRoutes {
+
+  // TODO: implement some sort of configuration
+
+  // TODO: organize routes into case classes
+  @cask.get("/")
+  def root() = {
+    // TODO: scalatags html should be contained in "views"
+    html(
+      body(
+        div(
+          h1(id:="title", "Chicxulub"),
+          p("Chicxulub blog")
+        )
+      )
+    )
   }
+
+  // TODO: define an actual logger, for some reason you can only call debug through this instance
+  override def main(args: Array[String]): Unit = {
+    log.debug("Starting Chicxulub server")
+    super.main(args)
+  }
+
+  // TODO: host needs to be configurable
+  override def host: String = "0.0.0.0"
+
+  initialize()
 }
